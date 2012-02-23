3b66b2c2756337bfc8b1df367fd1ba61bd20b9f7
Dima Pilipenko <dimap09@gmail.com>
2012-02-23 00:10:10 -0500

    Added project target file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Main.java
+ * 
+ *  This is file contains the Main method that should start execution
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//display welcome message
+		System.out.println("Welcome\n");
+
+	}
+
+}
